49fe48c5a7faad00f9f033de649c08aaabe35bcc
bosh44 <bosh44@gmail.com>
2017-12-08 22:45:57 +0200

    added some js side menu

---
+++ js/sidemenu.js
@@ -0,0 +1,17 @@
+function updates {
+  $('.updates').css({'display':'block'});
+  $('.about').css({'display':'none'});
+  $('.blog').css({'display':'none'});
+}
+
+function about {
+  $('.updates').css({'display':'none'});
+  $('.about').css({'display':'block'});
+  $('.blog').css({'display':'none'});
+}
+
+function blog {
+  $('.updates').css({'display':'none'});
+  $('.about').css({'display':'none'});
+  $('.blog').css({'display':'block'});
+}
