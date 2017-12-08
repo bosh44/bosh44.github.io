04d336cdbda431958380948b76e4de6100ccb194
bosh44 <bosh44@gmail.com>
2017-12-08 23:05:43 +0200

    added some js side menu

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,17 +1,17 @@
 function updates() {
-  document.getElementByClassName('updates').style.display='block';
-  document.getElementByClassName('about').style.display='none';
-  document.getElementByClassName('blog').style.display='none';
+  document.getElementsByClassName('updates').style.display='block';
+  document.getElementsByClassName('about').style.display='none';
+  document.getElementsByClassName('blog').style.display='none';
 }
 
 function about() {
-  document.getElementByClassName('updates').style.display='none';
-  document.getElementByClassName('about').style.display='block';
-  document.getElementByClassName('blog').style.display='none';
+  document.getElementsByClassName('updates').style.display='none';
+  document.getElementsByClassName('about').style.display='block';
+  document.getElementsByClassName('blog').style.display='none';
 }
 
 function blog() {
-  document.getElementByClassName('updates').style.display='none';
-  document.getElementByClassName('about').style.display='none';
-  document.getElementByClassName('blog').style.display='block';
+  document.getElementsByClassName('updates').style.display='none';
+  document.getElementsByClassName('about').style.display='none';
+  document.getElementsByClassName('blog').style.display='block';
 }
